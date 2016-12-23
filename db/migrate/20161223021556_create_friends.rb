b358a4324bf8bcf1412f37b045ef4da065948259
maangie <maangie@users.noreply.github.com>
2016-12-23 11:19:11 +0900

    Disallow the null of name

--- db/migrate/20161223021556_create_friends.rb
+++ db/migrate/20161223021556_create_friends.rb
@@ -1,7 +1,7 @@
 class CreateFriends < ActiveRecord::Migration[5.0]
   def change
     create_table :friends do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
