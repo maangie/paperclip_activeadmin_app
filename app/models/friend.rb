9f25aa9e20ca38b4097c1d94c4528b3eaed6ff2a
maangie <maangie@users.noreply.github.com>
2016-12-23 16:14:08 +0900

    Attachment must have validation

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,7 @@
 class Friend < ApplicationRecord
   has_attached_file :avatar
+
+  validates_attachment :avatar,
+                       content_type: { content_type: %w(image/jpg image/jpeg
+                                                        image/png image/gif) }
 end
