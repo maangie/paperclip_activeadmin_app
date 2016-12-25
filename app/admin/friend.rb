e9d0afc04b5ce18ed707c5b13dbf07785a90233d
maangie <maangie@users.noreply.github.com>
2016-12-25 13:42:37 +0900

    Allows friend to create, view, edit, and delete.

--- app/admin/friend.rb
+++ app/admin/friend.rb
@@ -1,17 +1,35 @@
+# https://spin.atomicobject.com/2016/07/23/file-upload-active-admin-paperclip/
 ActiveAdmin.register Friend do
+  permit_params :name, :avatar
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  index do
+    selectable_column
+    id_column
 
+    column :name
 
+    column 'Avatar', sortable: :avatar_file_name do |friend|
+      link_to friend.avatar_file_name, friend.avatar.url
+    end
+
+    column :avatar_file_size, sortable: :avatar_file_size do |friend|
+      "#{friend.avatar_file_size / 1024} KB"
+    end
+
+    column :created_at
+    actions
+  end
+
+  show do
+    image_tag friend.avatar.url
+  end
+
+  form do |f|
+    inputs do
+      f.input :name
+      f.input :avatar, required: true, as: :file
+    end
+
+    f.actions
+  end
 end
